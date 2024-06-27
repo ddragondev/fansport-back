63af49a3561c7c50027cc58832301f0ddcd75393
ddragondev <dvegamed@gmail.com>
2024-06-27 13:19:00 -0400

    Add likePost endpoint to post controller and routes

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,42 @@
 'use strict';
 
-/**
- * post controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async likePost(ctx) {
+    const postId = ctx.params.id;
+    const userId = ctx.state.user.id; // Asumiendo que el usuario está autenticado y su ID está en el estado
+
+    try {
+      const post = await strapi.entityService.findOne('api::post.post', postId, {
+        populate: { likes: true }
+      });
+
+      if (!post) {
+        return ctx.notFound('Post not found');
+      }
+
+      const userLikeIndex = post.likes.findIndex(like => like.id === userId);
+
+      if (userLikeIndex !== -1) {
+        // Si el usuario ya ha dado "like", quitar el "like"
+        post.likes.splice(userLikeIndex, 1);
+        const updatedPost = await strapi.entityService.update('api::post.post', postId, {
+          data: { likes: post.likes }
+        });
+
+        ctx.send(updatedPost);
+      } else {
+        // Si el usuario no ha dado "like", agregar el "like"
+        post.likes.push(userId);
+        const updatedPost = await strapi.entityService.update('api::post.post', postId, {
+          data: { likes: post.likes }
+        });
+
+        ctx.send(updatedPost);
+      }
+    } catch (error) {
+      ctx.throw(500, error);
+    }
+  },
+}));
--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
+const defaultRouter = createCoreRouter('api::post.post');
 
-module.exports = createCoreRouter('api::post.post');
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'POST',
+    path: '/posts/:id/like',
+    handler: 'post.likePost',
+    config: {
+      auth: { required: true }, // Modificación aquí
+      policies: [],
+      middlewares: [],
+    },
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
